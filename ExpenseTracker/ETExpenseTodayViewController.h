2ba2ed4a454511b80086403e2ea369ac739f89b0
Katreena Bacalso <k.bacalso@klab.com>
2014-06-18 19:54:11 +0800

    Removed commented code. Added Today view controller subclass.

---
+++ ExpenseTracker/ETExpenseTodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  ETExpenseTodayViewController.h
+//  ExpenseTracker
+//
+//  Created by Taki Bacalso on 6/18/14.
+//  Copyright (c) 2014 KlabCyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ETExpenseTodayViewController : UIViewController
+
+@end
